6869e455c2f853ecfa27ee1d319a0e7d7ac7f763
generalpaulo <generalpaulo098@gmail.com>
2019-09-13 18:39:04 -0300

    banco de dados

--- primeira/models.py
+++ primeira/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class CarrinhoDeCompras(models.Model):
+    Cliente = models.CharField(max_length=50)
+    idd = models.IntegerField()
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=50)
+    CarrinhoDeCompras = models.OneToOneField(CarrinhoDeCompras,on_delete=models.CASCADE,primary_key=True,)
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    codigo = models.IntegerField()
+    carrinho = models.ForeignKey(CarrinhoDeCompras, on_delete=models.CASCADE)
+
+
+
+
+
+
--- primeira/views.py
+++ primeira/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render,redirect
 
 def home(request):
     return render(request,'home.html')
+
+def list(request):
+    alunos = Aluno.objects.all()
+    return render(request, 'tt/list.html',{'alunos':alunos})
