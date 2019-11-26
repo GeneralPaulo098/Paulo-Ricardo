a50dda45032607f881a06304521becf64416752c
generalpaulo <generalpaulo098@gmail.com>
2019-11-26 20:29:51 -0300

    mensagem

--- primeira/urls.py
+++ primeira/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('carrinho/',views.carrinho),
-    path('cliente/', views.cliente),
     path('produto/', views.produto_list),
     path('produto/show/<int:id>/', views.produto_show),
     path('produto/deleta/<int:id>/', views.produto_delete),
     path('produto/editar/<int:id>/',views.editar),
     path('produto/create/', views.create),
+    path('produto/pesquisa/', views.pesquisa)
+ 
 
 ]
--- primeira/views.py
+++ primeira/views.py
@@ -1,42 +1,17 @@
 from django.shortcuts import render,redirect
 from . models import Produto
 from . forms import ProdutoForm
-from . models import CarrinhoDeCompras
-from . forms import CarrinhoDeComprasForm
+from django.db.models.fields import Field
+from django.db.models import IntegerField
+from django.contrib import messages
 
 def home(request):
     return render(request,'home.html')
 
-
-
-def carrinho(request):
-    if(request.method=='POST'):
-        form = ProdutoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/primeira/')
-        else:
-            return render(request,'carrinho/form.html',{'form':form})     
-    else:
-        form = ProdutoForm()
-        return render(request,'carrinho/form.html',{'form':form})
-
-
-def cliente(request):
-    if(request.method=='POST'):
-        form = ProdutoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/primeira/produto/')
-        else:
-            return render(request,'cliente/form.html',{'form':form})     
-    else:
-        form = ProdutoForm()
-        return render(request,'cliente/form.html',{'form':form})   
-
 def produto_list(request):
     produtos = Produto.objects.all()
-    return render(request, 'produto/list.html',{'produtos':produtos})
+    return render(request,'produto/list.html',{'produtos':produtos})
+
 
 def produto_show(request,id):
     produto = Produto.objects.get(pk=id)
@@ -65,9 +40,17 @@ def create(request):
         form = ProdutoForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.INFO, 'produto adicionado com sucesso!')
             return redirect('/primeira/produto/')
         else:
             return render(request,'produto/form.html',{'form':form})     
     else:
         form = ProdutoForm()
         return render(request,'produto/form.html',{'form':form})
+
+
+def pesquisa(request):
+    if (request.method=='POST'):
+        q = request.POST.get('pesquisar')   
+        produtos = Produto.objects.filter(nome__icontains=q)
+        return render(request,'produto/list.html',{'produtos':produtos})
